819dc3852733d6bff73bc1867a2f71e67bbb769b
soumyavaio <srj.soumya@gmail.com>
2018-04-05 09:56:03 +0530

    tatus deleted

--- Automation/seleniumfirefox/src/excel/LoginActitime.java
+++ Automation/seleniumfirefox/src/excel/LoginActitime.java
@@ -8,22 +8,24 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 public class LoginActitime {
 	public static void main(String[] args) throws Throwable {
-		String path="E:\\excel\\LoginActitime.xlsx";
+		String path = "/home/soumyavaio/phone.xls";
 		//
-		FileInputStream fis=new FileInputStream(path);
+		FileInputStream fis = new FileInputStream(path);
 		//
-		Workbook wb=WorkbookFactory.create(fis);
+		Workbook wb = WorkbookFactory.create(fis);
 		//
-		Sheet sh=wb.getSheet("Sheet1");
+		Sheet sh = wb.getSheet("Sheet");
 		//
-		Row row=sh.getRow(1);
+		Row row = sh.getRow(0);
 		//
-		String url=row.getCell(2).getStringCellValue();
-		String userName=row.getCell(3).getStringCellValue();
-		String Password=row.getCell(4).getStringCellValue();
+		String url = row.getCell(0).getStringCellValue();
+		String userName = row.getCell(1).getStringCellValue();
+		String password = row.getCell(2).getStringCellValue();
+		int i=Integer.parseInt(password);
 		System.out.println(url);
 		System.out.println(userName);
-		System.out.println(Password);
+		System.out.println(password);
+		System.out.println(i+1000);
 	}
 
 }
